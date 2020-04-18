fa200830fa6374daae966082e5d994c15f67ca43
Andika Prasetya <andikaandika866@gmail.com>
2020-04-18 20:55:19 +0700

    menambahkan mobile responsive

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,23 +3,48 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
     <title>kulinerBantul</title>
-    <link rel="stylesheet" href="<?= base_url('/assets/css/') ?>style.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('/assets/css/style.css') ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Pacifico&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <nav>
-        <div>
-            profil
+    <div class="container">
+        <nav>
+            <div class="logo">
+                KuliBan
+            </div>
+            <div class="nav-search">
+                <button class="fas fa-search"></button>
+                <input type="text" placeholder="    search...">
+            </div>
+            <div class="nav-menu">
+                <a class="fas fa-lg fa-home"></a>
+                <a class="fas fa-lg fa-bell"></a>
+                <a class="fas fa-lg fa-compass"></a>
+                <a><img src="<?= base_url('/assets/img/default.jpg') ?>"></a>
+            </div>
+        </nav>
+        <div class="content">
+            <div class="post">POST..</div>
+            <div class="event">EVENT...</div>
         </div>
-        <div>
-            profil
+        <img class="nav-img" src="<?= base_url('/assets/img/nav.jpg') ?>">
+        <div class="nav-gradient"></div>
+        <div class="mobile-menu">
+            <a class="fas fa-lg fa-home"></a>
+            <a class="fas fa-lg fa-bell"></a>
+            <a class="fas fa-lg fa-compass"></a>
+            <a><img src="<?= base_url('/assets/img/default.jpg') ?>"></a>
         </div>
-        <div>
-            profil
-        </div>
-    </nav>
+    </div>
+    <script>
+        document.querySelector('.logo').addEventListener('click', () => {
+            document.querySelector('.event').classList.toggle('show');
+        });
+    </script>
 </body>
 
 </html>
