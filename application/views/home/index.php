2a9cc8886e69c8a17f24b2ee9982eb96a431f0d7
Andika Prasetya <andikaandika866@gmail.com>
2020-04-18 12:35:19 +0700

    commit lagi

---
+++ application/views/home/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kulinerBantul</title>
+    <link rel="stylesheet" href="<?= base_url('/assets/css/') ?>style.css">
+</head>
+
+<body>
+    <nav>
+        <div>
+            profil
+        </div>
+        <div>
+            profil
+        </div>
+        <div>
+            profil
+        </div>
+    </nav>
+</body>
+
+</html>
