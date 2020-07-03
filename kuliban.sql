d03d0fcde68306ce89eb712b33f86ce6c0b19c33
Andika Prasetya <andikaandika866@gmail.com>
2020-07-03 20:11:59 +0700

    update database

--- kuliban.sql
+++ kuliban.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2020 at 04:42 AM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Generation Time: Jul 03, 2020 at 03:11 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -43,7 +42,11 @@ INSERT INTO `gambar` (`id_gambar`, `id_post`, `gambar`, `image_ref`) VALUES
 ('img1589769395', 'pst1589769327', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/img%2Fpost%2Ftempat-kuliner%2F1589769360477.jpg?alt=media&token=a3ea3160-820d-4ee6-b04e-e463a7103618', '/img/post/tempat-kuliner/1589769360473.jpg'),
 ('img1589769396', 'pst1589769327', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/img%2Fpost%2Ftempat-kuliner%2F1589769360473.jpg?alt=media&token=34d363aa-225d-4d08-93c0-fc4217c191a4', '/img/post/tempat-kuliner/1589769360477.jpg'),
 ('img1589769714', 'pst1589769544', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/img%2Fpost%2Fevent-kuliner%2F1589769705152.jpg?alt=media&token=80a80bf6-56a2-4e23-8805-5471e30f8bab', '/img/post/event-kuliner/1589769705152.jpg'),
-('img1589769715', 'pst1589769544', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/img%2Fpost%2Fevent-kuliner%2F1589769705156.PNG?alt=media&token=cfb8d5a3-f393-42ea-8d43-ea1c819b20d6', '/img/post/event-kuliner/1589769705156.PNG');
+('img1589769715', 'pst1589769544', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/img%2Fpost%2Fevent-kuliner%2F1589769705156.PNG?alt=media&token=cfb8d5a3-f393-42ea-8d43-ea1c819b20d6', '/img/post/event-kuliner/1589769705156.PNG'),
+('img1593688404', 'pst1593688367', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/img%2Fpost%2Ftempat-kuliner%2F1593688401715.jpg?alt=media&token=6b06e76f-d629-4d7d-bf2c-7512f3ca8d8d', '/img/post/tempat-kuliner/1593688401715.jpg'),
+('img1593688753', 'pst1593688722', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/img%2Fpost%2Fevent-kuliner%2F1593688750999.jpg?alt=media&token=c28268df-1a7c-41e4-9792-2049ef9c7c03', '/img/post/event-kuliner/1593688750999.jpg'),
+('img1593780244', 'pst1593780191', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/img%2Fpost%2Fevent-kuliner%2F1593780242411.jpg?alt=media&token=c9a1a4d4-6772-4e5f-8d76-2bb7de172a8f', '/img/post/event-kuliner/1593780242411.jpg'),
+('img1593781670', 'pst1593781626', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/img%2Fpost%2Ftempat-kuliner%2F1593781668209.jpg?alt=media&token=1ee2353f-92a5-42e1-b5ba-fca7c3e76085', '/img/post/tempat-kuliner/1593781668209.jpg');
 
 -- --------------------------------------------------------
 
@@ -83,7 +86,11 @@ CREATE TABLE `maps` (
 
 INSERT INTO `maps` (`id_map`, `id_post`, `latitude`, `longitude`) VALUES
 ('map1589769397', 'pst1589769327', '-7.887836005760962', '110.32744740551537'),
-('map1589769715', 'pst1589769544', '-7.888048552235762', '110.32814454408455');
+('map1589769715', 'pst1589769544', '-7.888048552235762', '110.32814454408455'),
+('map1593688404', 'pst1593688367', '-7.887474676503082', '110.3270398475826'),
+('map1593688753', 'pst1593688722', '-7.886345520537588', '110.3278876791591'),
+('map1593780245', 'pst1593780191', '-7.886345520537588', '110.3278876791591'),
+('map1593781670', 'pst1593781626', '-7.886422568924993', '110.32569919648799');
 
 -- --------------------------------------------------------
 
@@ -99,16 +106,20 @@ CREATE TABLE `post` (
   `alamat` varchar(300) NOT NULL,
   `konten` varchar(4000) NOT NULL,
   `jam_buka` varchar(30) DEFAULT NULL,
-  `tanggal` varchar(50) DEFAULT NULL
+  `tanggal` varchar(50) DEFAULT NULL,
+  `tanggal_posting` varchar(40) NOT NULL,
+  `approved` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `post`
 --
 
-INSERT INTO `post` (`id_post`, `id_user`, `id_jenis_post`, `judul_post`, `alamat`, `konten`, `jam_buka`, `tanggal`) VALUES
-('pst1589769327', 'usr1589769299', 'klr1587608474', 'Ayam Goreng Jawa Mbah Cemplung', 'askldjaslkdjaslkdj', 'alksjdalksjdlaksjd', '08:00 - 19:00', NULL),
-('pst1589769544', 'usr1589769299', 'evt1587608520', 'askldjaslkdjaslkdj', 'askldjaslkdjaslkdj', 'klsajdlksdjalskdjlaskjd', NULL, '2020-02-20');
+INSERT INTO `post` (`id_post`, `id_user`, `id_jenis_post`, `judul_post`, `alamat`, `konten`, `jam_buka`, `tanggal`, `tanggal_posting`, `approved`) VALUES
+('pst1593688367', 'usr1593688353', 'klr1587608474', 'warung hore', 'warung hore', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati distinctio facilis soluta fugiat corrupti illo perspiciatis neque magni nulla reprehenderit?', '20:20 - 20:20', '2020-02-20', '1593688404', 1),
+('pst1593688722', 'usr1587606716', 'evt1587608520', 'warung horee', 'warung hore', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati distinctio facilis soluta fugiat corrupti illo perspiciatis neque magni nulla reprehenderit?', '20:20 - 20:20', '2020-02-20', '1593688753', 1),
+('pst1593780191', 'usr1587606716', 'evt1587608520', 'warung horee', 'warung horeee', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati distinctio facilis soluta fugiat corrupti illo perspiciatis neque magni nulla reprehenderit?', NULL, '2020-02-20', '1593780245', 1),
+('pst1593781626', 'usr1587606716', 'klr1587608474', 'warung', 'warung hore', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, suscipit? Illo eos fugiat cumque nisi maxime ad quod hic corporis.', '20:20 - 20:20', NULL, '1593781670', 1);
 
 -- --------------------------------------------------------
 
@@ -155,8 +166,9 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id_user`, `nama_user`, `email`, `password`, `image`, `tanggal_lahir`, `telp`, `id_role`, `username`, `tanggal_daftar`, `ip_address`) VALUES
 ('usr1587606477', 'Andika Prasetya', 'andikaandika866@gmail.com', '$2y$10$Wk693gGhb9Lsqv2SwbtcOerZsz9FNoE5a5piO8AV2yx0.fGUNaj.W', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/user%2Fprofile%2Fdefault.jpg?alt=media&token=d9b82102-4eed-451d-8c89-23cd5692364d', '929829600', '089650941725', 'adm1587565941', 'andika1587606477', '1587606477', '0'),
-('usr1587606716', 'Andika Prasetya', 'andika12182546@bsi.ac.id', '$2y$10$KQ7.xife3zA9fUWQc5zQi.ACs3hujFSQHbus49SXJLR.rcAhrtrFO', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/user%2Fprofile%2Fdefault.jpg?alt=media&token=d6795d04-06b2-4d6e-abd6-51bf9b294001', '929829600', '089650941725', 'adm1587565941', 'andika1587606716', '1587606716', '0'),
-('usr1589769299', 'Andika Prasetya', 'andika@email.com', '$2y$10$SmoTpmO2wR92cwnkHlo.V.chbSRasU7AOZF9NJ6axSwqfdhvyV0Dm', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/user%2Fprofile%2FCapturasd.PNG?alt=media&token=7edd81a1-20cc-448a-a23d-bdade39f83f6', '929829600', '089913123123', 'adm1587565941', 'andika1589769299', '1589769299', '0');
+('usr1587606716', 'Andika Prasetya', 'andika12182546@bsi.ac.id', '$2y$10$pNin5qIT/Vp1.U76s5Y6keEmW04gYqNXcX1P8lqDbUvGZ.QYecfEe', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/user%2Fprofile%2Fdefault.jpg?alt=media&token=d6795d04-06b2-4d6e-abd6-51bf9b294001', '929829600', '089650941725', 'adm1587565941', 'andika1587606716', '1587606716', '0'),
+('usr1589769299', 'Andika Prasetya', 'andika@email.com', '$2y$10$SmoTpmO2wR92cwnkHlo.V.chbSRasU7AOZF9NJ6axSwqfdhvyV0Dm', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/user%2Fprofile%2FCapturasd.PNG?alt=media&token=7edd81a1-20cc-448a-a23d-bdade39f83f6', '929829600', '089913123123', 'adm1587565941', 'andika1589769299', '1589769299', '0'),
+('usr1593688353', 'admin', 'admin@email.com', '$2y$10$HBRb7rsNWeTg1S6o4m1aSOYwNyUiQuC17Mr5Z32fLmTl0KXkgSo/m', 'https://firebasestorage.googleapis.com/v0/b/kuliner-bantul.appspot.com/o/user%2Fprofile%2Fdefault.jpg?alt=media&token=09612b27-639a-44ea-be8d-4e12ad336e98', '929829600', '089650941725', 'mbr1587565962', 'admin1593688353', '1593688353', '0');
 
 --
 -- Indexes for dumped tables
